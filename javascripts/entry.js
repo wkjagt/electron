82a57d8cf7f18c1271d5288086ce02e09b0085bd
Willem van der Jagt <wkjagt@gmail.com>
2016-01-17 21:36:08 -0500

    learning flexbox from http://jsfiddle.net/aalbc/t5ee15y2/

--- javascripts/entry.js
+++ javascripts/entry.js
@@ -4,9 +4,14 @@ import React from "react";
 class MyComponent extends React.Component {
   render() {
     return (
-      <div className="myDiv">
-        Hello Electron!!!
-      </div>
+      <ul className="flex-container">
+        <li className="flex-item">1</li>
+        <li className="flex-item">2</li>
+        <li className="flex-item">3</li>
+        <li className="flex-item">4</li>
+        <li className="flex-item">5</li>
+        <li className="flex-item">6</li>
+      </ul>
     );
   }
 }
