639211704f4d6b16b5eb1140627bf5415facac78
Willem van der Jagt <wkjagt@gmail.com>
2016-01-19 21:38:34 -0500

    start adding some structure

--- javascripts/components/NotesApp.react.js
+++ javascripts/components/NotesApp.react.js
@@ -2,19 +2,37 @@ import React from "react";
 import Notebooks from "./NoteBooks.react";
 import Notes from "./Notes.react";
 import Editor from "./Editor.react";
+import NotesStore from "../stores/NotesStore";
 
 var {Component} = React;
 
-class App extends Component {
+class NotesApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = NotesStore.getAllNotes();
+  }
+
+  componentDidMount() {
+    NotesStore.addChangeListener(this._onChange);
+  }
+
+  componentWillUnmount() {
+    NotesStore.removeChangeListener(this._onChange);
+  }
+
+  _onChange() {
+    this.setState(NotesStore.getAllNotes());
+  }
+
   render() {
     return (
       <div className="flex-container">
         <Notebooks />
         <Notes />
-        <Editor />
+        <Editor content="haha" />
       </div>
     );
   }
 }
 
-module.exports = App;
+module.exports = NotesApp;
